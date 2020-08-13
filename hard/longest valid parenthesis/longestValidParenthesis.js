730e8b05b7b86de3dae4c810640617b96d19b05f
Vladislav Mogilevskiy <vmogilevskiy@Vladislavs-MacBook-Pro.local>
2020-08-12 19:29:01 -0700

    added valid parenthesis problem

--- hard/longest valid parenthesis/longestValidParenthesis.js
+++ hard/longest valid parenthesis/longestValidParenthesis.js
@@ -1,7 +1,26 @@
+// Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+// Example 1:
+
+// Input: "(()"
+// Output: 2
+// Explanation: The longest valid parentheses substring is "()"
+
+
+
+// Example 2:
+
+// Input: ")()())"
+// Output: 4
+// Explanation: The longest valid parentheses substring is "()()"
+
+
 /**
  * @param {string} s
  * @return {number}
  */
 var longestValidParentheses = function(s) {
-    
+    // need to identify valid parenthesis
+    // need to log length 
+    // longest stack length - final stack length?
 };
