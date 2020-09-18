988ab70f3d9b0c304967e5b02bede80ce7cad55b
Vladislav Mogilevskiy <vmogilevskiy@Vladislavs-MacBook-Pro.local>
2020-09-18 08:37:54 -0700

    finished add digits and started alternating bits

---
+++ easy/add digits/addDigits.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+    let i = 0
+    while (i < 100){
+        if (i%3 === 0) console.log(i)
+        i++
+    }
+};
+
+addDigits()
+// console.log(Math.floor(38/100))
+// console.log(Math.floor(38/10))
+// console.log(38%10)
+
