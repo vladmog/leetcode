8360ebab47ea7fc6d180f883b2664992535b0b55
Vladislav Mogilevskiy <vmogilevskiy@Vladislavs-MacBook-Pro.local>
2020-09-20 18:54:55 -0700

    completed add digits

--- easy/add digits/addDigits.js
+++ easy/add digits/addDigits.js
@@ -3,15 +3,19 @@
  * @return {number}
  */
 var addDigits = function(num) {
-    let i = 0
-    while (i < 100){
-        if (i%3 === 0) console.log(i)
-        i++
+    let numStr = num.toString()
+    while (numStr.length > 1){
+        let total = 0
+        for (let i = 0; i < numStr.length; i++){
+            total += parseInt(numStr[i])
+        }
+        numStr = total.toString()
     }
+    return Number(numStr)
 };
 
-addDigits()
+console.log(addDigits(99))
+
 // console.log(Math.floor(38/100))
 // console.log(Math.floor(38/10))
 // console.log(38%10)
-
